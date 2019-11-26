89f43683c060c5d186a98ed233a4a13a62a10626
LucianABC <louabecasis@gmail.com>
2019-11-26 15:16:59 -0300

    obtener precio maquina ok

--- pruebitas.test.js
+++ pruebitas.test.js
@@ -3,3 +3,12 @@ const vendedoras = funciones.vendedoras;
 const ventas = funciones.ventas;
 const precios = funciones.precios;
 const sucursales = funciones.sucursales;
+const obtenerIdVenta = funciones.obtenerIdVenta;
+const precioMaquina = funciones.precioMaquina;
+
+
+
+
+test('calcular monto', ()=>{
+    expect(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"])).toBe(320);
+});
