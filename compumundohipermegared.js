cf03c8babfe37d4c023260cc6c30ba8077c7df15
LucianABC <louabecasis@gmail.com>
2019-11-26 14:08:43 -0300

    Empezando el tp

---
+++ compumundohipermegared.js
@@ -0,0 +1,32 @@
+const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
+
+const ventas = [
+  [ 100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
+  [ 100000001, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
+  [ 100000002, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543', 'Motherboard MZI', 'HDD Toyiva'] ],
+  [ 100000003, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543', 'Motherboard ASUS 1200'] ],
+  [ 100000004, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000', 'Motherboard ASUS 1200'] ],
+  [ 100000005, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543', 'Motherboard ASUS 1200', 'RAM Quinston'] ]
+]
+
+const precios = [
+  [ 'Monitor GPRS 3000', 200 ],
+  [ 'Motherboard ASUS 1500', 120 ],
+  [ 'Monitor ASC 543', 250 ],
+  [ 'Motherboard ASUS 1200', 100 ],
+  [ 'Motherboard MZI', 30 ],
+  [ 'HDD Toyiva', 90 ],
+  [ 'HDD Wezter Dishital', 75 ],
+  [ 'RAM Quinston', 110 ],
+  [ 'RAM Quinston Fury', 230 ]
+];
+
+const sucursales = ['Centro', 'Caballito'];
+
+module.exports = {
+    vendedoras,
+    ventas,
+    precios,
+    sucursales
+
+}
---
+++ pruebitas.test.js
@@ -0,0 +1,5 @@
+const funciones = require('./compumundohipermegared.js');
+const vendedoras = funciones.vendedoras;
+const ventas = funciones.ventas;
+const precios = funciones.precios;
+const sucursales = funciones.sucursales;
